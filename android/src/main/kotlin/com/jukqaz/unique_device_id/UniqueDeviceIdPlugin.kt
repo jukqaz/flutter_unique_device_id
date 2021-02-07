f7da1af9ff8c43249561b039f98166ac87ce5091
jukqaz <jukqaz@gmail.com>
2021-02-08 04:07:06 +0900

    feat: add crypto

--- android/src/main/kotlin/com/jukqaz/unique_device_id/UniqueDeviceIdPlugin.kt
+++ android/src/main/kotlin/com/jukqaz/unique_device_id/UniqueDeviceIdPlugin.kt
@@ -1,9 +1,14 @@
 package com.jukqaz.unique_device_id
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Environment
 import android.provider.Settings
 import androidx.annotation.NonNull
+import androidx.security.crypto.EncryptedFile
+import androidx.security.crypto.MasterKey
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -14,15 +19,35 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
+import java.io.IOException
 import java.util.*
+
 
 /** UniqueDeviceIdPlugin */
 class UniqueDeviceIdPlugin : FlutterPlugin, MethodCallHandler {
+    companion object {
+        private val filePath = Environment.getExternalStorageDirectory().absolutePath
+        private const val fileName = "unique_device_id"
+    }
+
     private lateinit var channel: MethodChannel
     private var context: Context? = null
 
-    private val uuidFilePath
-        get() = context?.externalCacheDir?.path + "/unique_device_id"
+
+    private val encryptedFile by lazy {
+        context?.let {
+            val file = File(filePath, fileName)
+            val masterKey = MasterKey.Builder(it)
+                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                .build()
+            EncryptedFile.Builder(
+                it,
+                file,
+                masterKey,
+                EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
+            ).build()
+        }
+    }
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "unique_device_id")
@@ -34,7 +59,11 @@ class UniqueDeviceIdPlugin : FlutterPlugin, MethodCallHandler {
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
         when (call.method) {
             "getUniqueId" -> CoroutineScope(Dispatchers.Main).launch {
-                result.success(getUniqueId())
+                try {
+                    result.success(getUniqueId())
+                } catch (e: PermissionNotGrantedException) {
+                    result.error(e.localizedMessage, null, null)
+                }
             }
             else -> result.notImplemented()
         }
@@ -64,11 +93,21 @@ class UniqueDeviceIdPlugin : FlutterPlugin, MethodCallHandler {
     }
 
     private suspend fun readUUIDFromInternalStorage() = withContext(Dispatchers.IO) {
-        File(uuidFilePath).takeIf { it.exists() }?.readText(Charsets.UTF_8)
+        if (!checkExternalStoragePermission()) throw PermissionNotGrantedException()
+        try {
+            encryptedFile?.openFileInput()?.bufferedReader()?.use { it.readLine() }
+        } catch (e: IOException) {
+            null
+        }
     }
 
     private suspend fun writeUUIDToInternalStorage(uuid: String) = withContext(Dispatchers.IO) {
-        File(uuidFilePath).writeText(uuid, Charsets.UTF_8)
+        if (!checkExternalStoragePermission()) throw PermissionNotGrantedException()
+        try {
+            encryptedFile?.openFileOutput()?.bufferedWriter().use { it?.write(uuid) }
+        } catch (e: IOException) {
+            null
+        }
     }
 
     @SuppressLint("HardwareIds")
@@ -78,4 +117,9 @@ class UniqueDeviceIdPlugin : FlutterPlugin, MethodCallHandler {
             Settings.Secure.ANDROID_ID
         )
     }
+
+    private fun checkExternalStoragePermission() =
+        context?.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
 }
+
+class PermissionNotGrantedException : Exception()
